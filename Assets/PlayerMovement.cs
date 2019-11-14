516a71aab000c12c20a51d75f4b250f26ba747c7
Le0198 <lopezleonardo198@gmail.com>
2019-11-14 14:06:10 -0600

    Added Player
    Player movement is "ok" animations bad

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float walkSpeed;
+    public float jumpForce = 2.0f;
+    public float runSpeed;
+    private Vector3 jump;
+    private Rigidbody rb;
+    public bool isGrounded;
+    //private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //animator = GetComponent<Animator>();
+        rb = this.GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        jump = new Vector3(0.0f, jumpForce, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //  animator.setTrigger("standing");
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            isGrounded = false;
+            rb.AddForce(new Vector3(0.0f, 6.0f, 0.0f), ForceMode.Impulse);
+        }
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    private void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector3(move * runSpeed, rb.velocity.y, 0);
+    }
+
+
+}
