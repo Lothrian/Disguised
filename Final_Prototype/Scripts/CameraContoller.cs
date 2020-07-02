cd17294c0414f39041e2693a76e5a6cfe54f0eed
Allen Zhang <AllenZhang@AllenZhangtheGreat.local>
2020-07-02 01:37:19 -0500

    Add Prototype

---
+++ Final_Prototype/Scripts/CameraContoller.cs
@@ -0,0 +1,152 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraContoller : MonoBehaviour
+{
+    // This camera controller allows the camera to rotate around the player object and stick to it
+
+    [Header("Settings")]
+    public bool lockCursor;
+    public float mouseSensitiviy = 10;
+    public Transform target; // The player object
+    public float distFromTarget = 2;
+    public Vector2 pitchMinMax = new Vector2(-40, 85);
+
+    public float rotationSmoothTime = 8f;
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
+
+    float yaw;
+    float pitch;
+
+    [Header("Collision Vars")]
+
+    [Header("Transparancy")]
+    public bool changeTransparency = true;
+    public MeshRenderer targetRenderer;
+
+    [Header("Speeds")]
+    public float moveSpeed = 3;
+    public float returnSpeed = 9;
+    public float wallPush = 0.7f;
+
+    [Header("Distances")]
+    public float closestDistanceToPlayer = 2;
+    public float evenCloserDistanceToPlayer = 1;
+
+    [Header("Mask")]
+    public LayerMask collisionMask;
+
+    private bool pitchLock = false;
+
+    private void Start()
+    {
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        //Moves camera with player (target) and checks for collision
+        CollisionCheck(target.position - transform.forward * distFromTarget);
+        WallCheck();
+
+        //Rotates camera with mouse, only look at yaw if pitch is locked
+        if (!pitchLock) {
+            yaw += Input.GetAxis("Mouse X") * mouseSensitiviy;
+            pitch -= Input.GetAxis("Mouse Y") * mouseSensitiviy;
+            pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+            currentRotation = Vector3.Lerp(currentRotation, new Vector3(pitch, yaw), rotationSmoothTime * Time.deltaTime);
+        }
+        else {
+            yaw += Input.GetAxis("Mouse X") * mouseSensitiviy;
+            pitch = pitchMinMax.y;
+            currentRotation = Vector3.Lerp(currentRotation, new Vector3(pitch, yaw), rotationSmoothTime * Time.deltaTime);
+        }
+
+        transform.eulerAngles = currentRotation;
+
+        Vector3 e = transform.eulerAngles;
+        e.x = 0;
+
+        target.eulerAngles = e;
+    }
+
+    private void WallCheck() {
+        Ray ray = new Ray(target.position, -target.forward);
+        RaycastHit hit;
+
+        //If it detects a wall 
+        if(Physics.SphereCast(ray, 0.2f, out hit, 0.7f, collisionMask)) {
+            pitchLock = true;
+        }
+        else {
+            pitchLock = false;
+        }
+    }
+
+    private void CollisionCheck(Vector3 retPoint) {
+        RaycastHit hit;
+
+        //If camera hits object/wall, zoom in
+        if(Physics.Linecast(target.position, retPoint, out hit, collisionMask)) {
+            Vector3 norm = hit.normal * wallPush;
+            Vector3 p = hit.point + norm;
+
+            TransparencyCheck();
+
+            //If future predicted position is lower than our lowest possible distance, do nothing
+            if (Vector3.Distance(Vector3.Lerp(transform.position, p, moveSpeed * Time.deltaTime), target.position) <= evenCloserDistanceToPlayer) {
+
+            }
+            else {
+                transform.position = Vector3.Lerp(transform.position, p, moveSpeed * Time.deltaTime);
+
+            }
+            return;
+        }
+
+        //Not near a wall, reset camera and transparency
+        FullTransparency();
+
+        transform.position = Vector3.Lerp(transform.position, retPoint, returnSpeed * Time.deltaTime);
+        pitchLock = false;
+    }
+
+    private void TransparencyCheck() {
+        if (changeTransparency) {
+            if(Vector3.Distance(transform.position, target.position) <= closestDistanceToPlayer) {
+                Color tempCol = targetRenderer.sharedMaterial.color;
+                tempCol.a = Mathf.Lerp(tempCol.a, 0.2f, moveSpeed * Time.deltaTime);
+
+                targetRenderer.sharedMaterial.color = tempCol;
+                //If target has more than one material, loop through targetRenderer.sharedMaterials
+
+                
+            }
+            else {
+                if(targetRenderer.sharedMaterial.color.a <= 0.99f) {
+                    Color tempCol = targetRenderer.sharedMaterial.color;
+                    tempCol.a = Mathf.Lerp(tempCol.a, 1, moveSpeed * Time.deltaTime);
+
+                    targetRenderer.sharedMaterial.color = tempCol;
+                }
+            }
+        }
+    }
+
+    private void FullTransparency() {
+        if (changeTransparency) {
+            if (targetRenderer.sharedMaterial.color.a <= 0.99f) {
+                Color tempCol = targetRenderer.sharedMaterial.color;
+                tempCol.a = Mathf.Lerp(tempCol.a, 1, moveSpeed * Time.deltaTime);
+
+                targetRenderer.sharedMaterial.color = tempCol;
+            }
+        }
+    }
+}
---
+++ Final_Prototype/Scripts/PlayerMovement.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float walkSpeed;
+    public float runSpeed;
+
+    public float jumpForce = 2.0f;
+    private Vector3 jump;
+    private Rigidbody rb;
+    public bool isGrounded;
+    private Animator animator;
+    string dir = "right";
+    bool hasJumped = false;
+    float speedActivationTime = float.MinValue;
+    float invisActivationTime = float.MinValue;
+
+    float timer_period = 5;
+    public GameObject player;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        jump = new Vector3(0.0f, jumpForce, 0.0f);
+        animator = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Reset speed stats after "timer_period" seconds
+        if (Time.time > speedActivationTime + timer_period)
+        {
+            walkSpeed = 1;
+            runSpeed = 1;
+        }
+
+        // Reset opacity after "timer_period" seconds
+        if (Time.time > invisActivationTime + timer_period)
+        {
+            Color tempColor = player.GetComponent<SkinnedMeshRenderer>().materials[1].color;
+            Color tempColor2 = player.GetComponent<SkinnedMeshRenderer>().materials[0].color;
+            print(player.GetComponent<SkinnedMeshRenderer>().materials.Length);
+            tempColor.a = 1;
+            tempColor2.a = 1;
+            player.GetComponent<SkinnedMeshRenderer>().materials[1].color = tempColor;
+            player.GetComponent<SkinnedMeshRenderer>().materials[0].color = tempColor2;
+        }
+
+        // Check that the player is off the ground, have it set to 3 because player.y starts at 3
+        // (click on player object and check his initial position)
+        if (transform.position.y > 3)
+        {
+            hasJumped = true;
+        }
+
+        // Stuff for jumping and setting jumping animations
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                isGrounded = false;
+                rb.AddForce(new Vector3(0.0f, 6.0f, 0.0f), ForceMode.Impulse);
+            }
+        }
+
+        if (hasJumped) {
+            animator.SetBool("is_jumping", true);
+        } else {
+            animator.SetBool("is_jumping", false);
+        }
+
+        // Stuff for moving left/right, (instant rotation)
+        if (Input.GetKeyDown("a"))
+        {
+            if (dir.Equals("right"))
+            {
+                transform.RotateAround(transform.position, transform.up, 180f);
+            }
+            dir = "left";
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            if (dir.Equals("left"))
+            {
+                transform.RotateAround(transform.position, transform.up, 180f);
+            }
+            dir = "right";
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == ("Ground") && hasJumped){
+            hasJumped = false;
+        }
+        isGrounded = true;
+
+
+        // Collision detection with boosters, tags need to be set appropriately on objects
+        if (collision.gameObject.tag == ("SpeedBooster"))
+        {
+            collision.gameObject.SetActive(false);
+            runSpeed = 5;
+            walkSpeed = 5;
+            speedActivationTime = Time.time;
+        }
+
+        if (collision.gameObject.tag == ("InvisiblePickup"))
+        {
+            collision.gameObject.SetActive(false);
+            Color tempColor = player.GetComponent<SkinnedMeshRenderer>().materials[1].color;
+            Color tempColor2 = player.GetComponent<SkinnedMeshRenderer>().materials[0].color;
+            print(player.GetComponent<SkinnedMeshRenderer>().materials.Length);
+            tempColor.a = 0;
+            tempColor2.a = 0;
+            player.GetComponent<SkinnedMeshRenderer>().materials[1].color = tempColor;
+            player.GetComponent<SkinnedMeshRenderer>().materials[0].color = tempColor2;
+            invisActivationTime = Time.time;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Stuff for moving left/right + animations
+        float move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector3(move * runSpeed, rb.velocity.y, 0);
+        if (rb.velocity.x != 0) {
+            animator.SetBool("is_running", true);
+        }
+        else
+        {
+            animator.SetBool("is_running", false);
+        }
+    }
+
+
+}
